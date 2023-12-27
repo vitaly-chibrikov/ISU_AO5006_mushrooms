73a1a680005eb212d290dae5573169d021602579
Tully <vitaly.chibrikov@gmail.com>
2023-12-27 16:06:48 +0300

    verbose=True

--- mushrooms_clustering.py
+++ mushrooms_clustering.py
@@ -11,18 +11,23 @@ df=fetch_ucirepo(id=73)
 
 #Get features
 X=df.data.features
+#Fix NaNs
+#Features contain 2480 of NaNs in stalk-root. Remove bad column:
+X=X.drop(labels="stalk-root",axis=1)
+print("X shape after bad column removal: ", X.shape)
+
 LE=LabelEncoder()
 X=pd.DataFrame({col: LE.fit_transform(X[col]) for col in X}, index=X.index)
 
 inertias = []
 
 for i in range(1,31):
-    kmeans = KMeans(n_clusters=i)
+    kmeans = KMeans(n_clusters=i, n_init=10, verbose=True)
     kmeans.fit(X)
     inertias.append(kmeans.inertia_)
 
 plt.plot(range(1,31), inertias, marker='o')
-plt.title('Elbow method')
+plt.title('KMeans inertias')
 plt.xlabel('Number of clusters')
 plt.ylabel('Inertia')
 plt.show()
